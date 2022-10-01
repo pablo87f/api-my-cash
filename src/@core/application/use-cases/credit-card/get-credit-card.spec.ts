b237f12059adcd7c0f1986933d6c4796c59bd0a6
Pablo Silva <pablo.silva@avenueeco.com>
2022-10-01 10:19:46 -0300

    🧪 failing test to get credit card

--- src/@core/application/use-cases/credit-card/get-credit-card.spec.ts
+++ src/@core/application/use-cases/credit-card/get-credit-card.spec.ts
@@ -1,4 +1,5 @@
 import { CreditCard } from '../../../domain/entities/credit-card';
+import NotFoundError from '../../errors/not-found.error';
 import creditCardsRepositoryMock from '../../__mocks__/repositories/credit-cards-repository.mock';
 import GetCreditCard from './get-credit-card';
 
@@ -6,6 +7,10 @@ const makeSut = () => {
   const sut = new GetCreditCard(creditCardsRepositoryMock);
   return sut;
 };
+
+beforeEach(() => {
+  creditCardsRepositoryMock.get.mockClear();
+});
 
 describe('Get credit card', () => {
   it('should get a credit card from an user', async () => {
@@ -35,5 +40,28 @@ describe('Get credit card', () => {
     expect(creditCard).toBeInstanceOf(CreditCard);
     expect(creditCard.user_id).toEqual('user1');
     expect(creditCard.id).toEqual('creditcard1');
+  });
+
+  it('should throw an error when dont find the credit card from the specific user', async () => {
+    creditCardsRepositoryMock.get.mockResolvedValueOnce(undefined);
+
+    const sut = makeSut();
+    let error = undefined;
+
+    try {
+      await sut.execute({
+        credit_card_id: 'creditcard1',
+        user_id: 'user2',
+      });
+    } catch (e) {
+      error = e;
+    }
+
+    expect(creditCardsRepositoryMock.get).toHaveBeenCalledTimes(1);
+    expect(creditCardsRepositoryMock.get).toHaveBeenCalledWith(
+      'creditcard1',
+      'user2',
+    );
+    expect(error).toBeInstanceOf(NotFoundError);
   });
 });
